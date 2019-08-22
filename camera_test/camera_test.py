b3c8766517346a3fc0a2e9c80c3bc148737f465d
maz_leo <xtleon.xl@gmail.com>
2019-08-22 19:19:15 +0600

    videp properties docs link added

--- camera_test/camera_test.py
+++ camera_test/camera_test.py
@@ -1,4 +1,3 @@
-
 import cv2
 
 # print(cv2.__version__) # this is will print the version of opnev
@@ -11,13 +10,18 @@ if not cap.isOpened():
 
 # to capture continuous image we need infinite loop
 while True:
-    ret , frame = cap.read()
-    
+    ret, frame = cap.read()  # ret contains true/false and frame contains the image
+
     grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # GrayFrame
 
     # to show the image
     cv2.imshow('frame', frame)
     cv2.imshow('GrayFrame', grayFrame)  # Another frame with color gray
     # press q to exit window
-    if cv2.waitKey(20) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
+cap.release()
+cv2.destroyAllWindows()
+#  video capture properties
+#  link: https://docs.opencv.org/4.0.0/d4/d15/group__videoio__flags__base.html#gaeb8dd9c89c10a5c63c139bf7c4f5704d
