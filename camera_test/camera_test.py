38944d62d18bb2a70840f5457ffc77a593449aa8
maz_leo <xtleon.xl@gmail.com>
2019-08-22 19:06:25 +0600

    some changes

--- camera_test/camera_test.py
+++ camera_test/camera_test.py
@@ -1,23 +1,23 @@
-import numpy as np
+
 import cv2
 
 # print(cv2.__version__) # this is will print the version of opnev
 
-cap = cv2.VideoCapture(0)  #this is will capture image from webcam/camera
+cap = cv2.VideoCapture(0)  # this is will capture image from webcam/camera
 
 # Check camera open correct or not
-if cap.isOpened() == False:
+if not cap.isOpened():
     print('camera open fialed!')
 
 # to capture continuous image we need infinite loop
 while True:
     ret , frame = cap.read()
     
-    grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # GrayFrame 
+    grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # GrayFrame
 
     # to show the image
     cv2.imshow('frame', frame)
-    cv2.imshow('GrayFrame', grayFrame) # Another frame with color gray 
+    cv2.imshow('GrayFrame', grayFrame)  # Another frame with color gray
     # press q to exit window
     if cv2.waitKey(20) & 0xFF == ord('q'):
         break
