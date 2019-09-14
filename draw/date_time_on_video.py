eecae627517d6b47b90bbccd51e727382716caf1
maz_leo <xtleon.xl@gmail.com>
2019-09-15 00:54:59 +0600

    text and timedate added on video frame

--- draw/date_time_on_video.py
+++ draw/date_time_on_video.py
@@ -1,4 +1,5 @@
 import cv2
+import datetime
 
 cap = cv2.VideoCapture(0)
 
@@ -10,16 +11,16 @@ cap.set(4, 3000)  # 4 for height
 
 print(cap.get(3), cap.get(4))
 
-while(cap.isOpened()):
+while cap.isOpened():
     ret, frame = cap.read()
-
     if ret == True:
         font = cv2.FONT_HERSHEY_SIMPLEX;
-        text = "Some text on live video "
-        frame = cv2.putText(frame, text, (10, 50), font, 1, (0, 255, 255), 2, cv2.LINE_AA);
+        text = 'Width: '+str(cap.get(3)) +' Height: '+str(cap.get(4));
+        date_t = str(datetime.datetime.now());
+        frame = cv2.putText(frame, date_t, (10, 50), font, 1, (0, 255, 255), 2, cv2.LINE_AA);
         cv2.imshow('Frame ', frame)
 
-        if cv2.waitKey(1) & 0xFF == ord('c'):
+        if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     else:
         break
